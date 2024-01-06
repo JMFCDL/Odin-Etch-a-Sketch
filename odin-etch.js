24fd220e7f58317dc6c1e223d8914ad0309f5991
JMFCDL <148933609+JMFCDL@users.noreply.github.com>
2024-01-06 08:43:21 -0800

    add space to grids and hover effect

--- odin-etch.js
+++ odin-etch.js
@@ -1,15 +1,24 @@
-const total_num_grids = 16;
+const total_num_grids = 4;
 let count = 0;
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
+const box = document.getElementsByClassName("boxes");
 
 for(i = 0; i < total_num_grids; i++) {
     for(j = 0; j < total_num_grids; j++) {
         let grid = document.createElement("div");
+        grid.setAttribute("class", "boxes");
         count++;
-        grid.textContent = `hello + ${count}`;
+        grid.textContent = `${count}`;
         container.appendChild(grid);
     }
-    let breakline = document.createElement("p");
+    let breakline = document.createElement("div");
     breakline.setAttribute("class", "breaker");
     container.appendChild(breakline);
+}
+
+for(let i = 0; i < total_num_grids*total_num_grids; i++) {
+    box[i].style.height= "100px";
+    box[i].addEventListener("mouseover", event => {
+        box[i].style.background = "red";
+    });
 }
