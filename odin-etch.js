36ffdf964b3962ee5e878edf993cdfdd389d9eb9
JMFCDL <148933609+JMFCDL@users.noreply.github.com>
2024-01-22 21:06:59 -0800

    add user input with grid using buttons

--- odin-etch.js
+++ odin-etch.js
@@ -1,46 +1,106 @@
-const total_num_grids = 4;
+let total_num_grids = 4;
 let count = 0;
-let test = 1;
-let colorChange = "red";
-const containerGrid = document.querySelector("#container-grid");
+let gridHeight = 50;
+let temp = 0;
+let length = 0;
+let changeColor = "red";
+let storageVariable = (parseInt(localStorage.getItem("storageVariable")));
+
+
+const body = document.getElementById("etch-body");
+const containerSet = document.getElementById("container-settings");
+const containerGrid = document.getElementById("container-grid");
 const box = document.getElementsByClassName("boxes");
 
-for(i = 0; i < total_num_grids; i++) {
-    for(j = 0; j < total_num_grids; j++) {
+function gridCreation() {
+    console.log(`storage variable before everything is ${storageVariable}`)
+    if(isNaN(storageVariable) || storageVariable === " ") {
+        storageVariable = total_num_grids;
+    }
+    for(i = 0; i < storageVariable; i++) {
+        for(j = 0; j < storageVariable; j++) {
         let grid = document.createElement("div");
         grid.setAttribute("class", "boxes");
         count++;
         grid.textContent = `${count}`;
         containerGrid.appendChild(grid);
+        }
+        let breakline = document.createElement("div");
+        breakline.setAttribute("class", "breaker");
+        containerGrid.appendChild(breakline);
     }
-    let breakline = document.createElement("div");
-    breakline.setAttribute("class", "breaker");
-    containerGrid.appendChild(breakline);
 }
 
-for(let i = 0; i < total_num_grids*total_num_grids; i++) {
-    box[i].style.height= "50px";
+function gridHeightAdjustment() {
+    for(let i = 0; i < storageVariable*storageVariable; i++) {
+        box[i].style.height= gridHeight + "px";
+    }
 }
 
-containerGrid.addEventListener("click", changeBGC);
-
-function changeBGC() {
-    test++;
-    if(test % 2 != 0 && test > 0) {
-        for(let i = 0; i < total_num_grids*total_num_grids; i++) { //mouseover event is permanently inside for loop, figure out why, this is why remove event listener won't work
-            box[i].addEventListener("mouseover", function testing() {
-                console.log(i);
-                box[i].style.background = colorChange;
-            });
+function removeGrids() {
+    console.log(`in remove, storage is ${storageVariable}`);
+    for(i = 0; i < storageVariable; i++) {
+        for(j = 0; j < storageVariable; j++) {
+            box[0].remove();
         }
     }
-    if(test % 2 == 0 && test > 0) {
-        for(let i = 0; i < total_num_grids*total_num_grids; i++) { //mouseover event is permanently inside for loop, figure out why, this is why remove event listener won't work
-            box[i].addEventListener("mouseover", function testing() {
-                console.log(i);
-                    box[i].style.background = "";
-            });
-        }
-    }
-
 }
+
+//creating grid structure
+
+gridCreation();
+
+gridHeightAdjustment();
+
+containerGrid.addEventListener("click", event => { //color fill
+    for(let i = 0; i < total_num_grids*total_num_grids; i++) {
+        box[i].addEventListener("mouseover", event => {
+            box[i].style.background = changeColor;
+        });
+    }
+});
+
+//creating user settings
+let title = document.createElement("div");
+title.setAttribute("id", "title");
+title.textContent = "User Settings";
+containerSet.appendChild(title);
+
+let currentColor = document.createElement("div");
+currentColor.setAttribute("id", "current");
+currentColor.textContent = "Current Color: " + changeColor;
+containerSet.appendChild(currentColor);
+
+let decreaseGrid = document.createElement("button");
+decreaseGrid.setAttribute("id", "less-grid");
+decreaseGrid.textContent = "Less Grid";
+containerSet.appendChild(decreaseGrid);
+
+let increaseGrid = document.createElement("button");
+increaseGrid.setAttribute("id", "more-grid");
+increaseGrid.textContent = "Bigger Grid";
+containerSet.appendChild(increaseGrid);
+
+let resetButton = document.createElement("button");
+resetButton.setAttribute("id", "RB");
+resetButton.textContent = "Reset Grid";
+containerSet.appendChild(resetButton);
+
+containerSet.addEventListener("click", event => {
+    removeGrids();
+    count = 0;
+    switch(event.target.id) {
+        case "less-grid":
+            storageVariable--;
+            break;
+        case "more-grid":
+            storageVariable++;
+            break;
+        case "RB":
+            storageVariable = total_num_grids;
+            break;
+    }
+    gridCreation();
+    gridHeightAdjustment();
+    localStorage.setItem("storageVariable", storageVariable);
+});
