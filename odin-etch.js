0c5f4455440701dcde90f47787f1c027fad6096c
JMFCDL <148933609+JMFCDL@users.noreply.github.com>
2024-01-26 19:20:13 -0800

    add color buttons and outline adjustments

--- odin-etch.js
+++ odin-etch.js
@@ -1,19 +1,25 @@
 let total_num_grids = 4;
 let count = 0;
-let gridHeight = 50;
 let temp = 0;
 let length = 0;
 let changeColor = "red";
+let boxHeight = 50;
 let storageVariable = (parseInt(localStorage.getItem("storageVariable")));
+const gridHeight = 70;
+const gridWidth = 70;
+let check = false;
 
 
 const body = document.getElementById("etch-body");
 const containerSet = document.getElementById("container-settings");
 const containerGrid = document.getElementById("container-grid");
+const containerTitle = document.getElementById("container-title");
 const box = document.getElementsByClassName("boxes");
+const breaker = document.getElementsByClassName("breaker");
 
 function gridCreation() {
-    console.log(`storage variable before everything is ${storageVariable}`)
+    containerGrid.style.height = gridHeight + "vh";
+    containerGrid.style.width = gridWidth + "vw";
     if(isNaN(storageVariable) || storageVariable === " ") {
         storageVariable = total_num_grids;
     }
@@ -22,7 +28,6 @@ function gridCreation() {
         let grid = document.createElement("div");
         grid.setAttribute("class", "boxes");
         count++;
-        grid.textContent = `${count}`;
         containerGrid.appendChild(grid);
         }
         let breakline = document.createElement("div");
@@ -33,17 +38,47 @@ function gridCreation() {
 
 function gridHeightAdjustment() {
     for(let i = 0; i < storageVariable*storageVariable; i++) {
-        box[i].style.height= gridHeight + "px";
+        box[i].style.height= ((gridHeight/storageVariable)-0.30) + "vh";
     }
 }
 
 function removeGrids() {
-    console.log(`in remove, storage is ${storageVariable}`);
     for(i = 0; i < storageVariable; i++) {
         for(j = 0; j < storageVariable; j++) {
             box[0].remove();
         }
     }
+}
+
+function removeBreaker() {
+    for(i = 0; i < storageVariable; i++) {
+        breaker[0].remove();
+    }
+}
+
+function fillColor() {
+    for(let i = 0; i < storageVariable*storageVariable; i++) {
+        box[i].addEventListener("mouseover", event => {
+            if(event.buttons == 1 || event.buttons == 3) {
+                box[i].style.background = changeColor;
+            }
+        });
+    }
+}
+
+function eraserButtonClick() {
+    for(let i = 0; i < storageVariable*storageVariable; i++) {
+        box[i].addEventListener("mouseover", event => {
+            if(event.buttons == 1 || event.buttons == 3) {
+                box[i].style.background = "white";
+            }
+        });
+    }
+}
+
+function deleteGrid() {
+    removeBreaker(); 
+    removeGrids();
 }
 
 //creating grid structure
@@ -52,13 +87,12 @@ gridCreation();
 
 gridHeightAdjustment();
 
-containerGrid.addEventListener("click", event => { //color fill
-    for(let i = 0; i < total_num_grids*total_num_grids; i++) {
-        box[i].addEventListener("mouseover", event => {
-            box[i].style.background = changeColor;
-        });
-    }
-});
+fillColor();
+
+let gridTitle = document.createElement("div");
+gridTitle.setAttribute("id", "title-grid");
+gridTitle.textContent = "Grid";
+containerTitle.appendChild(gridTitle);
 
 //creating user settings
 let title = document.createElement("div");
@@ -68,39 +102,103 @@ containerSet.appendChild(title);
 
 let currentColor = document.createElement("div");
 currentColor.setAttribute("id", "current");
-currentColor.textContent = "Current Color: " + changeColor;
+currentColor.textContent = "Current Color: " + storageVariable*storageVariable;
 containerSet.appendChild(currentColor);
 
-let decreaseGrid = document.createElement("button");
-decreaseGrid.setAttribute("id", "less-grid");
-decreaseGrid.textContent = "Less Grid";
-containerSet.appendChild(decreaseGrid);
+let decreaseGridBoxes = document.createElement("button");
+decreaseGridBoxes.setAttribute("id", "less-boxes");
+decreaseGridBoxes.textContent = "Less boxes in grid";
+containerSet.appendChild(decreaseGridBoxes);
 
-let increaseGrid = document.createElement("button");
-increaseGrid.setAttribute("id", "more-grid");
-increaseGrid.textContent = "Bigger Grid";
-containerSet.appendChild(increaseGrid);
+let increaseGridBoxes = document.createElement("button");
+increaseGridBoxes.setAttribute("id", "more-boxes");
+increaseGridBoxes.textContent = "More boxes in grid";
+containerSet.appendChild(increaseGridBoxes);
 
-let resetButton = document.createElement("button");
-resetButton.setAttribute("id", "RB");
-resetButton.textContent = "Reset Grid";
-containerSet.appendChild(resetButton);
+let resetButtonBoxes = document.createElement("button");
+resetButtonBoxes.setAttribute("id", "RBB");
+resetButtonBoxes.textContent = "Reset Box Amount";
+containerSet.appendChild(resetButtonBoxes);
+
+let eraserButton = document.createElement("button");
+eraserButton.setAttribute("id", "eraserB");
+eraserButton.textContent = ("Eraser");
+containerSet.appendChild(eraserButton);
+
+let colorButton = document.createElement("button");
+colorButton.setAttribute("id", "colorB");
+colorButton.textContent = ("Color");
+containerSet.appendChild(colorButton);
+
+let blueButton = document.createElement("button");
+blueButton.setAttribute("id", "blueB");
+blueButton.textContent = ("Blue");
+containerSet.appendChild(blueButton);
+
+let redButton = document.createElement("button");
+redButton.setAttribute("id", "redB");
+redButton.textContent = ("Red");
+containerSet.appendChild(redButton);
+
+let blackButton = document.createElement("button");
+blackButton.setAttribute("id", "blackB");
+blackButton.textContent = ("Black");
+containerSet.appendChild(blackButton);
+
+let greenButton = document.createElement("button");
+greenButton.setAttribute("id", "greenB");
+greenButton.textContent = ("Green");
+containerSet.appendChild(greenButton);
+
 
 containerSet.addEventListener("click", event => {
-    removeGrids();
-    count = 0;
     switch(event.target.id) {
-        case "less-grid":
+        case "less-boxes":
+            deleteGrid();
             storageVariable--;
+            check = true;
             break;
-        case "more-grid":
+        case "more-boxes":
+            deleteGrid();
             storageVariable++;
+            check = true;
             break;
-        case "RB":
+        case "RBB":
+            deleteGrid();
             storageVariable = total_num_grids;
+            check = true;
+            break;
+        case "eraserB":
+            eraserButtonClick();
+            break;
+        case "colorB":
+            fillColor();
+            break;
+        case "blueB":
+            changeColor = "blue";
+            break;
+        case "redB":
+            changeColor = "red";
+            break;
+        case "blackB":
+            changeColor = "black";
+            break;
+        case "greenB":
+            changeColor = "green";
             break;
     }
-    gridCreation();
-    gridHeightAdjustment();
+    if(check === true) {
+        gridCreation();
+        gridHeightAdjustment();
+        fillColor();
+        check = false;
+    }
+    if(event.target.id === "eraserB") {
+        eraserButtonClick();
+    }
+    if(event.target.id === "colorB") {
+        fillColor();
+    }
     localStorage.setItem("storageVariable", storageVariable);
+    currentColor.textContent = "Current Color: " + changeColor;
 });
